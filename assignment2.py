0e614e6093295800ff97841389b9193537ffa18f
Charles Wambua <107111052+375516521@users.noreply.github.com>
2025-08-27 23:16:33 +0300

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,26 @@
 
+class Vehicle:
+    def move(self):
+        pass  # Placeholder (to be overridden by subclasses)
+
+class Car(Vehicle):
+    def move(self):
+        print("🚗 Driving on the road...")
+
+class Plane(Vehicle):
+    def move(self):
+        print("✈️ Flying in the sky...")
+
+class Boat(Vehicle):
+    def move(self):
+        print("⛵ Sailing on water...")
+
+class Bicycle(Vehicle):
+    def move(self):
+        print("🚴 Pedaling on the path...")
+
+# Demonstrating polymorphism
+vehicles = [Car(), Plane(), Boat(), Bicycle()]
+
+for v in vehicles:
+    v.move()  # Same method name, but different behavior!
